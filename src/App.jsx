53be54c1a8e269286380ffdb6c1b5f105e0c1101
Riajul-56 <riajulhasansiam@gmail.com>
2025-03-01 01:54:29 +0600

    Navbar section completed without responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import Rootlayout from "./layout/Rootlayout";
 import Navbar from "./section/Navbar";
-
 const App = () => {
   return (
+    // <Routes>
+    //   <Route element={<Rootlayout />}>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/AboutMe" element={<AboutMe />} />
+    //   </Route>
+    // </Routes>
     <>
-    <Navbar/>
+      <Navbar />
     </>
-  )
+  );
 };
 
 export default App;
--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import Container from "../components/Container";
 import logo from "../assets/logo.png";
+import Flex from "../components/Flex";
+import { NavLink } from "react-router";
+
+let menu = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About Me",
+    link: "/AboutMe",
+  },
+  {
+    name: "Protfolio",
+    link: "/Protfolio",
+  },
+  {
+    name: "Contact",
+    link: "/Contact",
+  },
+];
 
 const Navbar = () => {
   return (
-    <nav className="bg-primary_bg">
-        <Container>
-            <div>
-                <img src={logo}/>
-            </div>
-        </Container>
+    <nav className="bg-primary_bg py-4">
+      <Container>
+        <Flex className="gap-[219px] items-center">
+          <div>
+            <img src={logo} />
+          </div>
+
+          <menu>
+            <ul className="flex gap-[145px] text-primary_text">
+              {menu.map((item, index) => (
+                <li key={item.name}>
+                  <NavLink
+                    to={item.link}
+                    className={({ isActive }) =>
+                      isActive ? "navItem activeNavItem" : "navItem"
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </menu>
+        </Flex>
+      </Container>
     </nav>
   );
 };
